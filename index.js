df5e48ae41c81f287f40c62138aa3bdc78637852
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-09 13:18:23 +0000

    Copied Ticket 1 from Devtools to index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Linked!");
+
+//Ticket 1
+const id = document.getElementById('title');
+console.log(id);
+
+//Ticket 1b
+const className = document.getElementsByClassName('content');
+console.log(className)
+
+//Ticket 1c
+const tagName = document.getElementsByTagName('li');
+console.log(tagName);
+
+//Ticket 1d
+const querySelector = document.querySelector('.highlight');
+console.log(querySelector);
+
+//TICKET 2 
