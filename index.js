effad18ca8d5cf9ed6edc209689e8493ce1c787e
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-09 14:07:10 +0000

    ticket 2d complete

--- index.js
+++ index.js
@@ -16,4 +16,23 @@ console.log(tagName);
 const querySelector = document.querySelector('.highlight');
 console.log(querySelector);
 
-//TICKET 2 
+//TICKET 2a
+
+const itemList = document.getElementById('itemList').parentNode.nodeName;
+
+console.log(itemList);
+
+//Ticket 2b
+
+const childList = document.getElementById('itemList').childNodes;
+console.log(childList);
+
+//Ticket 2c
+const elementChildren = document.getElementById('itemList').children;
+console.log(elementChildren);
+
+//Ticket 2d
+
+const lastAndFirstChildren = document.getElementById("itemList").firstChild.lastChild;
+console.log(lastAndFirstChildren);
+
