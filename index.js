8e6916dda76a0d9bc921640556e1f246104cffc6
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-09 16:59:49 +0000

    ticket 3a to 3c complete

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ console.log(tagName);
 const querySelector = document.querySelector('.highlight');
 console.log(querySelector);
 
+//Ticket 1e
+
+const querySelectorAll = document.querySelectorAll('.highlight');
+console.log(querySelectorAll);
+
 //TICKET 2a
 
 const itemList = document.getElementById('itemList').parentNode.nodeName;
@@ -39,4 +44,32 @@ const lastChildren = document.getElementById('itemList').lastElementChild;
 console.log(firstChildren);
 console.log(lastChildren);
 
-// 
+// Ticket 3a
+
+const h1 = document.getElementById('title');
+h1.setAttribute("style", "color:red; font-size: 30px");
+
+console.log(h1);
+
+//Ticket 3b
+
+const body  = document.querySelector('body');
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "This is a dynamically added paragraph."
+
+body.appendChild(paragraph);
+
+console.log(paragraph);
+
+//Ticket 3c
+
+const content = document.querySelector('.content');
+content.textContent = "This paragraph has been updated.";
+
+console.log(content);
+
+//Ticket 3d
+
+//const secondPelement = document.querySelectorAll('.content' [1]);
+//secondPelement.setAttribute("title", "Hover over me!"); 
